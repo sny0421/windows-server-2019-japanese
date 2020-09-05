283bb67cc4b5a80de49e7e8e4a7d272ae8c5eb42
Yuto Sone <sny0421@Moonstone.local>
2020-09-05 17:11:11 +0900

    fix script

--- script/japanese_setting_2.ps1
+++ script/japanese_setting_2.ps1
@@ -22,6 +22,3 @@ reg load $DefaultHKEY $DefaultRegPath
 reg import "C:\Win10-ja-JP-default.reg"
 reg unload $DefaultHKEY
 reg import "C:\Win10-ja-JP-welcome.reg"
-
-Remove-Item "C:\Win10-ja-JP-default.reg"
-Remove-Item "C:\Win10-ja-JP-welcome.reg"
